2a1f541ad90ed40f5d05de0547c230c2fb86f4b3
siddiqueayesha02 <ayeshasiddique1102@gmail.com>
2024-03-20 04:05:44 +0530

    task

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <div>
+            <h1>TechStar</h1>
+            <p>@all right reserved</p>
+        </div>
+        
+        <div>
+            <h5>Follow us</h5>
+            <div>
+                <a href="https://www.youtube.com/channel/UCUzxn3A0LxUd4I7xExTPy2A" target={"blank"}>YouTube</a>
+                <a href="https://www.linkedin.com/in/ayesha-siddique-99244724a" target={"blank"}>linkedin</a>
+                <a href="https://github.com/siddiqueayesha02" target={"blank"}>Github</a>
+            </div>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer
